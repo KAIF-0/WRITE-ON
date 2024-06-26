c17ac431a8d4ef72411be3b7cb47c7c01f2830e7
KAIF 0 <kaif8700979251@gmail.com>
2024-06-26 08:10:39 +0530

    edit functinality fixed

--- frontend/src/assets/UserComments.jsx
+++ frontend/src/assets/UserComments.jsx
@@ -93,7 +93,7 @@ const UserComments = ({ comment, onLike, onEdit, onDelete }) => {
 
 
                         <div className="flex items-center justify-between mt-2 space-x-2">
-                            <button className={`flex items-center text-xs ${comment.likes.includes(presentUser._id) ? "text-blue-700" : " text-blue-700" } hover:text-blue-500`} onClick={() => { onLike(comment._id) }}>
+                            <button className={`flex items-center text-xs ${comment.likes.includes(presentUser._id) ? "text-blue-700" : " text-gray-400" } hover:text-blue-500`} onClick={() => { onLike(comment._id) }}>
                                 <svg className='h-4 w-4 mr-1' fill="currentColor" viewBox="0 0 20 20">
                                     <path d="M2 10.5a1.5 1.5 0 113 0v6a1.5 1.5 0 01-3 0v-6zM6 10.333v5.43a2 2 0 001.106 1.79l.05.025A4 4 0 008.943 18h5.416a2 2 0 001.962-1.608l1.2-6A2 2 0 0015.56 8H12V4a2 2 0 00-2-2 1 1 0 00-1 1v.667a4 4 0 01-.8 2.4L6.8 7.933a4 4 0 00-.8 2.4z" />
                                 </svg>
