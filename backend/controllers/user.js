19b96973ebf82ae65ef5077592409aa38da409f4
KAIF 0 <kaif8700979251@gmail.com>
2024-06-27 15:07:48 +0530

    User Validation Edited

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -10,21 +10,15 @@ export const updateUser = async (req, res, next) => {
   }
 
   if (req.body.password) {
-    if (req.body.password < 6) {
+    if (req.body.password < 4) {
       return next(errorHandler(400, "Password must be at least 6 charecter."))
     }
     req.body.password = bcryptjs.hashSync(req.body.password, 10)
   }
 
   if (req.body.username) {
-    if (req.body.username < 7 || req.body.username > 20) {
+    if (req.body.username < 3 || req.body.username > 20) {
       return next(errorHandler(400, "Username must be between 7 to 20 charecter."))
-    }
-    if (req.body.username.includes(' ')) {
-      return next(errorHandler(400, "Username cannot contain spaces."))
-    }
-    if (req.body.username !== req.body.username.toLowerCase()) {
-      return next(errorHandler(400, "Username must be in lowercase."))
     }
     if (!req.body.username.match(/^[a-zA-Z0-9]+$/)) {
       return next(errorHandler(400, "Username must only contain letter and numbers."))
